6b0b8f3d7a035bb996d1076d8a410fe720607e3f
admitriyev <inbox@dmitriyev.name>
2014-01-15 06:31:36 -0700

    Added Rails app with Product
    Web app is in sharp_app folder

---
+++ web/sharp_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SharpApp::Application.config.secret_key_base = '9940a3d8fe434e11b49cf85dff2861dc7612f2177e98a787166e986aedb08082828556cf58afd3d44faadce4104e5be9d5e9e2b8c6311ddd48c58d412b36832f'
---
+++ web/sharp_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SharpApp::Application.config.session_store :cookie_store, key: '_sharp_app_session'
