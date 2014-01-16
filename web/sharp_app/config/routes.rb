bd8c45ab5606b152bc2e374e1da05a1622317789
admitriyev <inbox@dmitriyev.name>
2014-01-15 21:40:45 -0700

    added routes and Twitter bootstrap

--- web/sharp_app/config/routes.rb
+++ web/sharp_app/config/routes.rb
@@ -5,7 +5,7 @@ SharpApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,7 +14,7 @@ SharpApp::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :products
 
   # Example resource route with options:
   #   resources :products do
