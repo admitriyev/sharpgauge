a9de2e379fa9b36f0c3b6c47621f4523acb77af1
admitriyev <inbox@dmitriyev.name>
2014-01-16 05:52:12 -0700

    java octave sample

--- services/src/sharp_back/ModelOne.java
+++ services/src/sharp_back/ModelOne.java
@@ -1,10 +1,24 @@
 package sharp_back;
 
+import dk.ange.octave.OctaveEngine;
+import dk.ange.octave.OctaveEngineFactory;
+import dk.ange.octave.type.Octave;
+import dk.ange.octave.type.OctaveDouble;
+import dk.ange.octave.type.OctaveString;
+
 public class ModelOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("main");
+		OctaveEngine octave = new OctaveEngineFactory().getScriptEngine();
+		 octave.put("fun", new OctaveString("sqrt(1-t**2)"));
+		 octave.put("t1", Octave.scalar(0));
+		 octave.put("t2", Octave.scalar(1));
+		 octave.eval("result = lsode(fun, 0, [t1 t2])(2);");
+		 OctaveDouble result = octave.get(OctaveDouble.class, "result");
+		 octave.close();
+		 double integral = result.get(1);
+		 System.out.print(integral);
+		 //assertEquals(Math.PI / 4, integral, 1e-5);	
 	}
 
 }
